97641e8e6c3be11103ec0213a2d424dd8d9c5c95
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-07-10 16:57:13 +0900

    MVC for bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,11 +1,28 @@
 # frozen_string_literal: true
 
 class BookmarksController < ApplicationController
+  before_action :find_list, only: %i[new create]
   def new
-
+    @bookmark = Bookmark.new
   end
 
   def create
+    @bookmark = Bookmark.new(set_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
 
+  private
+
+  def find_list
+    @list = List.find(params[:list_id])
+  end
+
+  def set_params
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
