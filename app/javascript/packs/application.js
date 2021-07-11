2979bfe640ad76cf7770605fc294cd27037101cd
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-07-12 07:08:38 +0900

    Add type.js and also packages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,7 @@ import '../controllers/index';
 import $ from 'jquery';
 import 'jquery-bar-rating';
 import 'select2';
-
+import Typed from 'typed.js';
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -30,5 +30,21 @@ document.addEventListener('turbolinks:load', () => {
  $('#bookmark_movie_id').select2({
     selectOnClose: true
   });
+
+  const initTyped = () => {
+    const element = document.querySelector('.typed-js');
+    if (element) {
+      new Typed('.typed-js', {
+        strings: ['^800 All of your favorites in one place. ^600\n', 'Drama', 'Classic', 'Comedy', 'Action', 'Romantic', 'Thriller', 'Horror', 'Comedy', 'Sci-fi', 'Slasher ^70 ... ^400'],
+        // strings: ['Drama', 'Classic', 'Comedy', 'Action', 'Romantic', 'Thriller', 'Horror', 'Slasher'],
+        typeSpeed: 30,
+        backSpeed: 16,
+        loop: true,
+        loopCount: Infinity,
+        showCursor: false,
+      });
+    }
+  };
+  initTyped();
 });
 
