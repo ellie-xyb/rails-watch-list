bc53418934e77904f8c237410f3c5ffe81e5dfa6
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-07-13 22:44:14 +0900

    modify

--- db/migrate/20210710025807_create_lists.rb
+++ db/migrate/20210710025807_create_lists.rb
@@ -1,11 +1,11 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
 class CreateLists < ActiveRecord::Migration[6.0]
-  def change
-    create_table :lists do |t|
-      t.string :name
-      t.string :bg_url
-      t.timestamps
-    end
-  end
+#   def change
+#     create_table :lists do |t|
+#       t.string :name
+#       t.string :bg_url
+#       t.timestamps
+#     end
+#   end
 end
